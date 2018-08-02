b77b36ac04d45e5bd61eb0aefef1b87d27602510
jlyoon <jerry1455@gmail.com>
2018-08-02 11:38:21 +0900

    Add files via upload
    web camera capture program using qroilib

---
+++ qroicapture/app/main.cpp
@@ -0,0 +1,13 @@
+#include <QApplication>
+#include <QDebug>
+
+// Local
+#include "mainwindow.h"
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+    MainWindow* window = new MainWindow();
+    window->show();
+    return app.exec();
+}
